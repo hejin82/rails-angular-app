0d40e11c7b2ce7cb7055636892262812daf78900
hejin <hejin82@gmail.com>
2020-04-04 19:30:14 +0900

    add loader

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,10 @@
 const { environment } = require('@rails/webpacker')
 const typescript =  require('./loaders/typescript')
+const html = require('./loaders/html');
+const sass = require('./loaders/sass');
 
 environment.loaders.prepend('typescript', typescript)
+environment.loaders.prepend('html', html);
+environment.loaders.prepend('sass', sass);
+
 module.exports = environment
